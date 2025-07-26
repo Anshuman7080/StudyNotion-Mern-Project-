571db548fe45adfc803dc71436ad117a1b135851
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2025-07-26 16:14:04 +0530

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -15,10 +15,3 @@ const rootReducer = combineReducers({
 
 export default rootReducer;
 
-//........................combineReducers....................
-//  combineReducers is a function from Redux Toolkit that helps you
-//  organize multiple reducers into one big root reducer.
-//In Redux, the state of your application is managed by reducers. 
-// But in a large app, you usually have multiple reducers, each handling
-// different parts of the state (like authentication, user profile, cart, courses, etc.).
-// Instead of manually merging them, combineReducers helps combine them into one object.
