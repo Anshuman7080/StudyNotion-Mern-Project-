b6e651e966683e1d8a9b12918f410245cbf5d390
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2025-07-26 16:03:24 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -8,10 +8,7 @@ const courseRoutes = require('./routes/Course');
 const database = require('./config/database');
 const cookieParser = require('cookie-parser');
 const cors = require('cors'); 
-// backend entertain the front request
-// CORS stands for Cross-Origin Resource Sharing. It's a way to allow web pages from one 
-// website (or origin) to request resources (like data or files) from a different website (or origin). 
-// Think of it like giving permission for one website to talk to another.
+
 const { cloudinaryConnect } = require('./config/cloudinary');
 const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
@@ -19,16 +16,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-// Database connect
+
 database.connect();
 
-// Middleware
-app.use(express.json());
-// This line sets up your application to handle JSON data.
-//  It means that when your server receives requests with JSON data in 
-// the body, it can automatically parse that data and make it available in your code.
 
-// simpally ,it converts the json data into a JavaScript object that you can work with.
+app.use(express.json());
 
 app.use(cookieParser());
 
@@ -38,11 +30,9 @@ app.use(
         credentials: true,
     })
 );
-// origin: 'http://localhost:3000': This means only requests coming from 
-// http://localhost:3000 are allowed.
+
+// http://localhost:3000 
 // https://studynotion-mern-project-frontend.onrender.com
-// credentials: true: This allows cookies and other credentials
-//  (like authentication tokens) to be sent along with the requests.
 
 
 app.use(
@@ -52,16 +42,16 @@ app.use(
     })
 );
 
-// Cloudinary connection
+
 cloudinaryConnect();
 
-// Routes
+
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 
-// Default route
+
 app.get("/", (req, res) => {
     return res.json({
         success: true,
